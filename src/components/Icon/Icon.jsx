96fe327d7d91215826a743d773fa0f319e8298df
Alex Goliansky <a_goliansky@viyar.ua>
2024-09-17 00:09:35 +0300

    fix icons

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -3,7 +3,7 @@ import css from './Icon.module.css';
 
 const Icon = ({ name, className = '', active = false, ...props }) => (
   <svg className={clsx(css.icon, css[className], { [css.active]: active })} {...props}>
-    <use xlinkHref={`/src/assets/icons.svg#${name}`} />
+    <use xlinkHref={`/icons.svg#${name}`} />
   </svg>
 );
 
